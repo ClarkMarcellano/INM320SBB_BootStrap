b95e305643ba5f5cb0d3f8da7f2481804c274338
ClarkMarcellano <c_marcellano@hotmail.com>
2025-04-14 18:48:25 -0400

    Detailing

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,21 +1,29 @@
-fetch('assets/js/cards.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('infoCardsContainer');
+async function loadCards() {
+  try {
+    const fetchedData = await fetch('./assets/js/cards.json');
+    const result = await fetchedData.json();
+    console.log('Fetched result:', result);
 
-    data.forEach(card => {
+    const cardContainer = document.querySelector('#infoCardsContainer');
+
+    result.forEach(card => {
       const cardHTML = `
         <div class="col-md-3">
-          <div class="card text-center border-start border-${card.borderColor} border-4">
+        <div class="card text-center border">
             <div class="card-body">
-              <h6 class="card-subtitle mb-2 text-muted">${card.subtitle}</h6>
-              <h3 class="card-title">${card.value}</h3>
               <p class="fw-bold">${card.title}</p>
+              <h3 class="card-title">${card.value}</h3>
             </div>
           </div>
         </div>
       `;
-      container.innerHTML += cardHTML;
+
+      cardContainer.innerHTML += cardHTML;
     });
-  })
-  .catch(error => console.error('Error loading JSON:', error));
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+  }
+}
+
+loadCards();
+
